a17a66e4fa0e8292252dc19e45e7189f1f83e5b3
teemvat <teemu.vataja@metropolia.fi>
2024-11-08 21:17:41 +0200

    Navbariin lisätty nappi toimii

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
+  
     <div>
       <Navbar />
       <Hero />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
 import logo from '../assets/images/logo.svg'
 import PageLinks from './PageLinks'
 
+
 const Navbar = () => {
+
+  function handleButton() {
+    document.getElementById("nav-content").classList.toggle("hidden")
+  }
+
   return (
     <div>
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <img src={logo} className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars" />
-            </button>
-          </div>
-          <PageLinks parentClass='nav-links' itemClass='nav-link' />
-          <ul className="nav-icons">
+      <header className="flex items-center justify-between flex-wrap py-4 w-full bg-yellow-200">
+        <div className="flex shrink-0 ml-6 cursor-pointer">
+          <span className="text-3xl font-bold text-green-700">FixHub</span>
+        </div>
+
+        <button
+          id="nav-toggle" onClick={handleButton}
+          class="md:hidden p-2 mr-4 ml-6 my-2 border rounded border-gray-600 text-black hover:border-blue-200">
+          <i class="fas fa-bars fa-2x"></i>
+        </button>
+        <div className="pl-6 w-full md:w-auto hidden md:block" id="nav-content">
+          <PageLinks parentClass='md:flex'
+            itemClass='mr-6 p-1 text-green-700 hover:text-blue-300' />
+          <ul className="md:flex">
             <li>
               <a
                 href="https://www.facebook.com"
@@ -46,7 +56,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </nav>
+      </header>
     </div>
 
   );
