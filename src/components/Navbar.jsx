5cd4c78d52416ba9212e562de42d4628d8b57bfa
teemvat <teemu.vataja@metropolia.fi>
2024-11-11 19:48:26 +0200

    tailwind config väripresetit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,51 +10,19 @@ const Navbar = () => {
 
   return (
     <div>
-      <header className="flex items-center justify-between flex-wrap py-4 w-full bg-yellow-200">
+      <header className="flex items-center justify-between flex-wrap py-4 w-full bg-fh_dgreen">
         <div className="flex shrink-0 ml-6 cursor-pointer">
-          <span className="text-3xl font-bold text-green-700">FixHub</span>
+          <span className="text-4xl font-bold font-sans text-fh_beige">FixHub</span>
         </div>
 
         <button
           id="nav-toggle" onClick={handleButton}
-          class="md:hidden p-2 mr-4 ml-6 my-2 border rounded border-gray-600 text-black hover:border-blue-200">
+          class="md:hidden p-2 mr-4 ml-6 my-2 border rounded border-gray-600 text-fh_beige hover:border-blue-200">
           <i class="fas fa-bars fa-2x"></i>
         </button>
         <div className="pl-6 w-full md:w-auto hidden md:block" id="nav-content">
           <PageLinks parentClass='md:flex'
-            itemClass='mr-6 p-1 text-green-700 hover:text-blue-300' />
-          <ul className="md:flex">
-            <li>
-              <a
-                href="https://www.facebook.com"
-                target="_blank"
-                rel="noreferrer"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.x.com"
-                target="_blank"
-                rel="noreferrer"
-                className="nav-icon"
-              >
-                <i className="fab fa-x-twitter" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.squarespace.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace" />
-              </a>
-            </li>
-          </ul>
+            itemClass='mr-6 p-1 text-fh_beige hover:text-fh_beige-light text-2xl' />
         </div>
       </header>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,48 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    colors: {
+      'fh_black': {
+        DEFAULT:'#39373b',
+        light:'#464349',
+        dark:'#28272a',
+      },
+      'fh_dgreen': {
+        DEFAULT:'#3e5e32',
+        light:'#4c753b',
+        dark:'#334a2a',
+      },
+      'fh_lgreen': {
+        DEFAULT:'#849161',
+        light:'#98a378',
+        dark:'#5f6a46',
+      },
+      'fh_yellow': {
+        DEFAULT:'#d69d30',
+        light:'#e2bb62',
+        dark:'#c88b2a',
+      },
+      'fh_beige': {
+        DEFAULT:'#d6c69a',
+        dark:'#c5a972', 
+        light:'#e6ddc2',
+      },
+      'fh_white': {
+        DEFAULT:'#efefe8',
+        light:'#f8f8f4',
+        dark:'#dcdccc',
+      },
+
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Roboto Slab', 'serif'],
+    },
+
+    extend: {
+      
+    },
   },
   plugins: [],
 }
