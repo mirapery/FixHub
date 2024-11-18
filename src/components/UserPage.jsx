9c674941509891873d21759782ec8175f49e9eeb
teemvat <teemu.vataja@metropolia.fi>
2024-11-18 20:05:21 +0200

    userpage tab content and route customization

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import UserFull from "./UserFull.jsx"
-import { dummyFixer } from "../data";
+import { dummyUsers } from "../data";
+import { useParams } from "react-router-dom";
 
 
 
 const UserPage = () => {
+    const { userId } = useParams();
+    const user = dummyUsers.find((u) => u.id === parseInt(userId, 10));
+
+    if (!user) {
+        return (
+            <div className="bg-fh_white">
+            <div className=" p-4">
+                <p className="text-fh_black text-2xl">
+                    User not found
+                </p>
+            </div>
+        </div>
+        )
+    }
 
     return (
         <div className="bg-fh_white">
             <div className=" p-4">
-                <UserFull userData={dummyFixer[0]} />
+                <UserFull userData={user} />
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -15,32 +15,78 @@ export const socialLinks = [
 // dummy itemi
 
 export const dummyItem = [
-  { id: 1, 
-    userId: 27, 
-    name: "Kenkä", 
-    description: "Reikä on juu.", 
-    category: "Clothing", 
-    location: "Uusimaa, Helsinki, 00790", 
-    priceRange: "3e-5e", 
-    date_publish: "13/11/2024", 
-    images: ["kenkä.jpg", "kenkä2.jpg"] 
+  {
+    id: 1,
+    userId: 27,
+    name: "Kenkä",
+    description: "Reikä on juu.",
+    category: "Clothing",
+    location: "Uusimaa, Helsinki, 00790",
+    priceRange: "3e-5e",
+    date_publish: "13/11/2024",
+    images: ["kenkä.jpg", "kenkä2.jpg"],
+    isFixed: "false",
+    fixerId: "none"
   },
 ]
 
 export const dummyFixer = [
-  { id: 1, 
-    userName: "raica", 
-    name: "Raikka Pulkkinen", 
-    phone: "0401234567", 
-    email: "raica@hotmail.com", 
-    password: "Salasana123", 
-    image: "userPic1.jpg", 
-    creationTime: "01/01/2024", 
-    location:"31400, Somero, Varsinais-Suomi", 
-    isFixer: "true", 
-    about:"I like to fix things", 
-    reviewCount: 2, 
-    reviewAverage: 4.5, 
-    tags: ["cars", "electronics"], 
-    fixedCount:0}
+  {
+    id: 1,
+    userName: "raica",
+    name: "Raikka Pulkkinen",
+    phone: "0401234567",
+    email: "raica@hotmail.com",
+    password: "Salasana123",
+    image: "userPic1.jpg",
+    creationTime: "01/01/2024",
+    location: "31400, Somero, Varsinais-Suomi",
+    isFixer: "true",
+    about: "I like to fix things",
+    reviewCount: 2,
+    reviewAverage: 4.5,
+    tags: ["cars", "electronics"],
+    fixedCount: 0
+  }
+]
+
+export const dummyUsers = [
+  {
+    id: 1,
+    userName: "raica",
+    name: "Raikka Pulkkinen",
+    phone: "0401234567",
+    email: "raica@hotmail.com",
+    password: "Salasana123",
+    image: "userPic1.jpg",
+    creationTime: "01/01/2024",
+    location: "31400, Somero, Varsinais-Suomi",
+    isFixer: "true",
+    about: "I like to fix things",
+    reviewCount: 2,
+    reviewAverage: 4.5,
+    tags: ["cars", "electronics"],
+    fixedCount: 0
+  },
+  {
+    id: 2,
+    userName: "juuseri",
+    name: "Pertti Eräreikä",
+    phone: "0406664242",
+    email: "pertti@hotmail.com",
+    password: "Passwurd666",
+    creationTime: "05/01/2024",
+    location: "00100, Helsinki, Uusimaa",
+    isFixer: "false",
+  }
+]
+
+export const dummyReview = [
+  {
+    id: 1,
+    fixerId: 1,
+    reviewerId: 2,
+    rating: 4,
+    body: "Good work pölöplöplöplöö. Juupajuu, hehheh."
+  }
 ]
