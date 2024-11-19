3dfd516c864bf5e7cb4d6af7c25b5a841c3ca16f
teemvat <teemu.vataja@metropolia.fi>
2024-11-19 20:32:11 +0200

    smooth scrolling to the top of page

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    smoothScrollToTop();
+  }, [pathname]);
+
+  function smoothScrollToTop() {
+    const scrollDuration = 300; // ms
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  }
+
+  return null;
+}
+
+export default ScrollToTop;
