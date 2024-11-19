0622ab73301718ac7c24cfe55526a6d4e8d1cd6b
Mira <mira.peryshko@metropolia.fi>
2024-11-20 01:05:03 +0200

    Add logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,10 @@
-// Custom middleware to log incoming requests
 const logger = (req, res, next) => {
     const currentDate = new Date().toISOString();
     console.log(`[${currentDate}] ${req.method} request to ${req.url}`);
+    res.on('finish', () => {
+        console.log(`[${currentDate}] ${req.method} ${req.url} - ${res.statusCode}`);
+    });
     next();
 };
 
-module.exports = logger;
+export default logger;
