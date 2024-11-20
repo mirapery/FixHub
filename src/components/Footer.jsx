27843f1385d549ee86a11b6aeb586cc98abfc944
Ville <villesc@gmail.com>
2024-11-20 22:17:42 +0200

    self assesment made week2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import SocialLinks from './SocialLinks';
 
 const Footer = () => {
   return (
-    <footer className=" bottom-0 flex items-center justify-between flex-wrap py-4 w-full bg-fh_dgreen">
+    <footer className=" bottom-0  flex items-center justify-between flex-wrap py-4 w-full bg-fh_dgreen">
       <div className="pl-6 w-full md:w-auto  md:block" id="nav-content">
         <SocialLinks parentClass='flex m-2'
           itemClass='mr-10 p-1 h-full text-fh_beige hover:text-fh_beige-light text-3xl' />
