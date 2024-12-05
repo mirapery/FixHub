c01bc59a8c698bcc67d4212fb15c18f9abc21e4b
teemvat <teemu.vataja@metropolia.fi>
2024-12-05 15:12:59 +0200

    review fix

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { dummyUsers } from "../assets/data";
 import { Link } from "react-router-dom";
 import EditReviewWindow from "./EditReviewWindow";
