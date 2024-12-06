e5e6ddc7b461397c24bed6fc2ec17f8f58bec435
Ville <villesc@gmail.com>
2024-12-07 00:34:37 +0200

    fixed homescreen text

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -9,10 +9,10 @@ function Hero() {
     >
       
           <div
-            className=" text-center  bg-fh_dgreen-dark bg-opacity-30 p-10 w rounded-md md:m-20 lg:mr-60 md:mr-40  
+            className=" flex text-center items-center  bg-fh_dgreen-dark   bg-opacity-30 h-full md:h-auto rounded-md md:m-20 lg:mr-60 md:mr-40  
           "
           >
-        <h1 className="text-4xl text-fh_beige " >
+        <h1 className="text-4xl text-fh_beige  " >
             Korjauta tavarasi edullisesti
             <br />
             Tai
@@ -20,8 +20,8 @@ function Hero() {
             Ansaitse korjaamalla muiden rikkinäisiä tavaroita
         </h1>
           </div>
-        <div className="hidden md:block flex items-center ">
-        <div className=" text-center  w-[30vh] h-[30vh] text-fh_beige bg-fh_dgreen bg-opacity-30 rounded-xl">
+        <div className="hidden md:flex items-center text-center  ">
+        <div className="  w-[30vh] h-[30vh] text-fh_beige bg-fh_dgreen bg-opacity-30 rounded-xl">
           <h1 className="text-3xl">BEST</h1>
           <p>most veri nise servise</p>
           <p className="mt-10 text-6xl">*****</p>
