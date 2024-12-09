7cb6535831d7955ed3cbc887c6503ee46ed7f368
Mira <mira.peryshko@metropolia.fi>
2024-12-09 18:59:48 +0200

    Use requireAuth middleware in routes

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const { getAllItems, getItemById, createItem, updateItem, deleteItem } = require('../controllers/itemController.js');
 const validateObjectId = require("../middleware/validateObjectId.js");
+const requireAuth = require('../middleware/requireAuth.js');
 
 const router = express.Router();
 
-// GET /api/items
+// GET ALL ITEMS (GET /api/items)
 router.get('/', getAllItems);
 
-// POST /api/items
-router.post('/', createItem);
-
-// GET /api/items/:itemId
+// GET ONE ITEM (GET /api/items/:itemId)
 router.get('/:itemId', validateObjectId('itemId'), getItemById);
 
-// PATCH /api/items/:itemId
-router.patch('/:itemId', validateObjectId('itemId'), updateItem);
+// CREATE ITEM (POST /api/items)
+router.post('/', requireAuth, createItem);
 
-// DELETE /api/items/:itemId
-router.delete('/:itemId', validateObjectId('itemId'), deleteItem);
+// EDIT ITEM (PATCH /api/items/:itemId)
+router.patch('/:itemId', requireAuth, validateObjectId('itemId'), updateItem);
+
+// DELETE ITEM (DELETE /api/items/:itemId)
+router.delete('/:itemId', requireAuth, validateObjectId('itemId'), deleteItem);
 
 module.exports = router;
