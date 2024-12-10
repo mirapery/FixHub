224f65c8439f9817b2510ef627cddd59cb01d6a3
Eetu Oinonen <eetuoi@metropolia.fi>
2024-12-11 00:44:04 +0200

    atlas change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const { DB_URI } = require('./config.js');
 const process = require('node:process');
+const { MongoClient } = require('mongodb');
 
-// PLACEHOLDER connection script - replace later
+
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(DB_URI);
-        console.log('Connected to the database');
+        const conn = await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error('Failed to connect to the database', error);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
