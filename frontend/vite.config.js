7b846cdcaca4acef0a716d4747dfb3068461f80e
teemvat <teemu.vataja@metropolia.fi>
2024-12-11 14:25:15 +0200

    proxytest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
         target: 'https://fixhub-be.onrender.com',
         changeOrigin: true,
         secure: false,
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Proxying request:', req.url);
+          });
+        },
       },
     },
   },
