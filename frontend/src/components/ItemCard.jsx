6195f7a8acec0b5817e50dc3b2b12a23659bca0a
Ville <villesc@gmail.com>
2024-12-11 09:45:20 +0200

    itemcard

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -33,32 +33,35 @@ const ItemCard = ({ itemData }) => {
     }
   };
 
-  return (
-    <Link
-      to={itemLink()}
-      className="flex  flex-none flex-col m-4 p-4 items-center rounded-md bg-fh_beige min-w-120 active:scale-95 hover:brightness-75 hover:cursor-pointer hover:shadow-lg hover:scale-105 transition duration-300"
-    >
-      <img
-        src={
-          itemData.image ? `http://localhost:5173/api/users/${itemData._id}/image/0`:
-          itemData.images
-            ? `http://localhost:5173/api/items/${itemData._id}/image/0`
-            : "/src/assets/images/itemPlaceholder.jpg"
-        } // tähän eetulta lopullinen versio
-        alt={itemData.name}
-        className="w-32 h-32 object-cover m-4 rounded-md"
-      />
-      <h2 className="text-fh_black font-serif font-bold text-2xl m-2">
-        {itemData.name}
-      </h2>
-      <p className="text-fh_black m-1">{formattedPrice || formattedTags}</p>
-      <div className="flex flex-row my-2 text-fh_black text-lg">
-        <i className="fa-solid fa-location-dot mr-2" />
-        <p className="mr-1">{itemData.location.province},</p>
-        <p>{itemData.location.city}</p>
-      </div>
-    </Link>
-  );
+  const imageURL = itemData.isFixer
+  ? (itemData.images
+      ? `http://localhost:5173/api/users/${itemData._id}/image`
+      : "/src/assets/images/userPlaceholder.jpg")
+  : (itemData.images
+      ? `http://localhost:5173/api/items/${itemData._id}/image/0`
+      : "/src/assets/images/itemPlaceholder.jpg");
+
+return (
+  <Link
+    to={itemLink()}
+    className="flex  flex-none flex-col m-4 p-4 items-center rounded-md bg-fh_beige min-w-120 active:scale-95 hover:brightness-75 hover:cursor-pointer hover:shadow-lg hover:scale-105 transition duration-300"
+  >
+    <img
+      src={imageURL} // tähän eetulta lopullinen versio
+      alt={itemData.name}
+      className="w-32 h-32 object-cover m-4 rounded-md"
+    />
+    <h2 className="text-fh_black font-serif font-bold text-2xl m-2">
+      {itemData.name}
+    </h2>
+    <p className="text-fh_black m-1">{formattedPrice || formattedTags}</p>
+    <div className="flex flex-row my-2 text-fh_black text-lg">
+      <i className="fa-solid fa-location-dot mr-2" />
+      <p className="mr-1">{itemData.location.province},</p>
+      <p>{itemData.location.city}</p>
+    </div>
+  </Link>
+);
 };
 
 export default ItemCard;
